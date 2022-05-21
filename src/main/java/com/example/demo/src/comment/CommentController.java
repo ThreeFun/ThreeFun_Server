52ef30c5400d27b6b0daba4d38538bd3f7e0f0ee
choiyeeun1010 <yeeun201010@naver.com>
2022-05-22 04:24:56 +0900

    create 댓글 등록 API

--- src/main/java/com/example/demo/src/comment/CommentController.java
+++ src/main/java/com/example/demo/src/comment/CommentController.java
@@ -1,4 +1,54 @@
 package com.example.demo.src.comment;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.config.BaseResponseStatus;
+import com.example.demo.src.comment.CommentProvider;
+import com.example.demo.src.comment.CommentService;
+import com.example.demo.src.comment.model.*;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.*;
+import static com.example.demo.utils.ValidationRegex.isRegexEmail;
+
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final CommentProvider commentProvider;
+    @Autowired
+    private final CommentService commentService;
+    @Autowired
+    private final JwtService jwtService;
+
+    public CommentController(CommentProvider commentProvider, CommentService commentService, JwtService jwtService){
+        this.commentProvider = commentProvider;
+        this.commentService = commentService;
+        this.jwtService = jwtService;
+    }
+
+    /**
+     * 댓글 등록 API
+     * [POST] /comments
+     * @return BaseResponse<Integer>
+     */
+    // Body
+    @ResponseBody
+    @PostMapping("")
+    public BaseResponse<Integer> createComment(@RequestBody PostComment postComment) {
+        try{
+            int commentIdx = commentService.createComment(postComment);
+            return new BaseResponse<>(commentIdx);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
 }
--- src/main/java/com/example/demo/src/comment/CommentDao.java
+++ src/main/java/com/example/demo/src/comment/CommentDao.java
@@ -1,4 +1,29 @@
 package com.example.demo.src.comment;
 
+import com.example.demo.src.comment.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
 public class CommentDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public int createComment(PostComment postComment){
+        String createCommentQuery = "insert into Comment (comment, teamIdx, userIdx) values (?, ?, ?);";
+        Object[] createCommentParams = new Object[]{postComment.getComment(), postComment.getTeamIdx(), postComment.getUserIdx()};
+        this.jdbcTemplate.update(createCommentQuery, createCommentParams);
+
+        String lastInserIdQuery = "select last_insert_id()";
+        return this.jdbcTemplate.queryForObject(lastInserIdQuery,int.class);
+    }
 }
